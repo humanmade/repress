55bca8e16ebf03a20dbc2cfaf017d8afd4bbdfa9
Ryan McCue <me@ryanmccue.info>
2018-03-08 13:45:21 +1000

    Add HOC for single posts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as handler } from './handler';
 export { parseResponse, mergePosts } from './utilities';
 export { default as withArchive } from './withArchive';
+export { default as withSingle } from './withSingle';
---
+++ src/withSingle.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { resolve } from './utilities';
+
+export default ( handler, getSubstate, mapPropsToId ) => Component => {
+	class WrappedComponent extends React.Component {
+		componentWillMount() {
+			if ( ! this.props.posts && ! this.props.loading ) {
+				this.props.onLoad();
+			}
+		}
+
+		componentWillReceiveProps( nextProps ) {
+			if ( ! nextProps.posts && this.props.postId !== nextProps.postId ) {
+				nextProps.onLoad();
+			}
+		}
+
+		render() {
+			return <Component { ...this.props } />;
+		}
+	}
+
+	const mapStateToProps = ( state, props ) => {
+		const substate = getSubstate( state );
+		const resolvedId = resolve( mapPropsToId, props );
+		const post = handler.getSingle( substate, resolvedId );
+
+		return {
+			post,
+			postId: resolvedId,
+			loading: handler.isPostLoading( substate, resolvedId ),
+		};
+	};
+
+	const mapDispatchToProps = ( dispatch, props ) => {
+		const resolvedId = resolve( mapPropsToId, props );
+		return {
+			onLoad: ( context = 'view' ) => dispatch( handler.fetchSingle( resolvedId, context ) ),
+		};
+	};
+
+	return connect(
+		mapStateToProps,
+		mapDispatchToProps
+	)( WrappedComponent );
+}
